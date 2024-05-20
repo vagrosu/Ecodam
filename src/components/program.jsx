8a812ae420d8396be15acb21e8e5555f45d7bcc3
vagrosu <vagrosu@yahoo.com>
2024-05-20 14:45:20 +0300

    Update program data

--- src/components/program.jsx
+++ src/components/program.jsx
@@ -6,14 +6,20 @@ export const Program = (props) => {
       <div className="container">
         <div>
           <div style={{textAlign: "center"}}>
-            <h1 style={{fontSize:"5em",}}>ECODAM 2024 Schedule</h1>
+            <h1 style={{fontSize: "5em"}}>ECODAM 2024 Programme</h1>
+            <h2 style={{fontSize: "3em"}}>Ferdinand Hall, University Rectorate</h2>
+
             <h2>Tuesday, June 11</h2>
             <ul>
               <li><strong>09:00</strong> Registration – in front of the Senate Hall</li>
               <li><strong>09:30 – 10:00</strong> Session I. ECODAM 2024 Opening</li>
-              <li><strong>10:00 – 11:00</strong> Session II. Kenneth DeJong (George Mason University): The Evolution of Evolutionary Computation</li>
+              <li><strong>10:00 – 11:00</strong> Session II. Kenneth DeJong (George Mason University): The Evolution of
+                Evolutionary Computation
+              </li>
               <li><strong>11:00 – 11:15</strong> Coffee break</li>
-              <li><strong>11:15 – 13:15</strong> Session III. Zbigniew Michalewicz (University of Adelaide): On the Significance of Dragon Years in the Life of a Scientist</li>
+              <li><strong>11:15 – 13:15</strong> Session III. Zbigniew Michalewicz (University of Adelaide): On the
+                Significance of Dragon Years in the Life of a Scientist
+              </li>
               <li><strong>13:30</strong> Lunch</li>
               <li><strong>16:00 – 18:00</strong> My PhD thesis in three minutes. PhD students’ short presentations</li>
               <li><strong>18:30</strong> Gala dinner</li>
@@ -21,30 +27,46 @@ export const Program = (props) => {
 
             <h2>Wednesday, June 12 – Satellite Workshop</h2>
             <ul>
-              <li><strong>10:00 – 11:30</strong> Bioinformatics and Applied Machine Learning Workshop. Research projects. Co-organised with the Core Bioinformatics Team, Wellcome-MRC Cambridge Stem Cell Institute, University of Cambridge</li>
+              <li><strong>10:00 – 11:30</strong> Bioinformatics and Applied Machine Learning Workshop. Research
+                projects. Co-organised with the Core Bioinformatics Team, Wellcome-MRC Cambridge Stem Cell Institute,
+                University of Cambridge
+              </li>
               <li><strong>11:30 – 11:45</strong> Coffee break</li>
-              <li><strong>11:45 – 13:15</strong> Bioinformatics and Applied Machine Learning Workshop. Research projects</li>
+              <li><strong>11:45 – 13:15</strong> Bioinformatics and Applied Machine Learning Workshop. Research projects
+              </li>
               <li><strong>13:30</strong> Lunch</li>
-              <li><strong>16:00 – 18:00</strong> Bioinformatics and Applied Machine Learning Workshop. Research projects</li>
+              <li><strong>16:00 – 18:00</strong> Bioinformatics and Applied Machine Learning Workshop. Research projects
+              </li>
             </ul>
 
             <h2>Thursday, June 13</h2>
             <ul>
-              <li><strong>10:00 – 11:00</strong> Session IV. Sorin Istrail (Brown University): Combinatorial and Statistical Prediction of Gene Expression from Haplotype Sequence</li>
+              <li><strong>10:00 – 11:00</strong> Session IV. Sorin Istrail (Brown University): Combinatorial and
+                Statistical Prediction of Gene Expression from Haplotype Sequence
+              </li>
               <li><strong>11:00 – 11:30</strong> Coffee break</li>
-              <li><strong>11:30 – 12:30</strong> Session V. Irina Mohorianu (Cambridge University): In data we trust. Machine Learning approaches reveal answers to unasked questions</li>
+              <li><strong>11:30 – 12:30</strong> Session V. Irina Mohorianu (University of Cambridge): In data we trust.
+                Machine Learning approaches reveal answers to unasked questions
+              </li>
               <li><strong>13:00</strong> Lunch</li>
-              <li><strong>15:00 – 16:00</strong> Session VI. Panel discussion. Where do I publish? Speed vs. Quality</li>
+              <li><strong>15:00 – 16:00</strong> Session VI. Panel discussion. Where do I publish? Speed vs. Quality
+              </li>
               <li><strong>16:00 – 18:00</strong> Guided tour of Iasi City</li>
             </ul>
 
             <h2>Friday, June 14</h2>
             <ul>
-              <li><strong>10:00 – 11:00</strong> Session VII. Gabriela Ochoa (University of Stirling): Optimisation Trajectories and Landscapes: A Complex Networks View</li>
+              <li><strong>10:00 – 11:00</strong> Session VII. Gabriela Ochoa (University of Stirling): Optimisation
+                Trajectories and Landscapes: A Complex Networks View
+              </li>
               <li><strong>11:00 – 11:30</strong> Coffee break</li>
-              <li><strong>11:30 – 12:30</strong> Session VIII. Dan Simovici (University of Massachusetts): Monotonic Entropies - Distribution Measures for Partitions of Structured and Unstructured Data with Applications</li>
+              <li><strong>11:30 – 12:30</strong> Session VIII. Dan Simovici (University of Massachusetts): Monotonic
+                Entropies - Distribution Measures for Partitions of Structured and Unstructured Data with Applications
+              </li>
               <li><strong>13:00</strong> Lunch</li>
-              <li><strong>15:00 – 16:00</strong> Session IX. Daniela Zaharie (West University Timisoara): Scheduling problem features and selection of metaheuristics components</li>
+              <li><strong>15:00 – 16:00</strong> Session IX. Daniela Zaharie (West University of Timișoara): Scheduling
+                problem features and selection of metaheuristics components
+              </li>
               <li><strong>16:00</strong> Session X. ECODAM 2024 Closing</li>
             </ul>
           </div>
