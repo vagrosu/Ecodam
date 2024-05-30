decde5c4196aa0f7d2a12070ba610e7cbd394e69
vagrosu <vagrosu@yahoo.com>
2024-05-30 19:30:02 +0300

    Add maps

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,56 +2,79 @@ import React from "react";
 import styled from "styled-components";
 
 const Section = styled.div`
-  padding: 100px 0 60px;
-  background: linear-gradient(to right, #6372ff 0%, #5ca9fb 100%);
-  color: rgba(255, 255, 255, 0.75);
+    padding: 100px 0 40px;
+    background: linear-gradient(to right, #6372ff 0%, #5ca9fb 100%);
+    color: rgba(255, 255, 255, 0.75);
 `;
 
 const Container = styled.div`
-  max-width: 1000px;
-  margin: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `;
 
 const SectionTitle = styled.h2`
-  color: #fff;
-  margin-top: 10px;
-  margin-bottom: 15px;
-  padding-bottom: 15px;
-  position: relative;
+    color: #fff;
+    margin-top: 10px;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    position: relative;
 
-  &::after {
-    position: absolute;
-    content: "";
-    background: rgba(255, 255, 255, 0.3);
-    height: 4px;
-    width: 60px;
-    bottom: 0;
-    left: 0;
-  }
+    &::after {
+        position: absolute;
+        content: "";
+        background: rgba(255, 255, 255, 0.3);
+        height: 4px;
+        width: 60px;
+        bottom: 0;
+        left: 0;
+    }
 `;
 
 const ContactItem = styled.div`
-  margin-bottom: 20px;
+    margin-bottom: 20px;
 `;
 
 const ContactLabel = styled.span`
-  display: block;
-  font-weight: bold;
-  margin-bottom: 5px;
+    display: block;
+    font-weight: bold;
+    margin-bottom: 5px;
 `;
 
 const ContactText = styled.p`
-  margin: 0;
-  font-size: 18px;
+    margin: 0;
+    font-size: 18px;
 `;
 
 const Footer = styled.div`
-  background: #000;
-  color: #fff;
-  text-align: center;
+    background: #000;
+    color: #fff;
+    text-align: center;
+    padding: 20px 0;
+`;
+
+const IframeContainer = styled.div`
+  width: 100%;
+  height: 410px;
+  overflow: hidden;
+
+  iframe {
+    width: 100%;
+    height: 480px;
+    border: none;
+    margin-top: -70px;
+  }
+`;
+
+const iframeHtml = `
+  <iframe 
+    src="https://www.google.com/maps/d/u/0/embed?mid=1MfPvgzi8MeqdEnLBV6dvyxUR7u5OU7c&ehbc=2E312F&noprof=1&ll=47.175376, 27.579017&z=14.9" 
+    style="border: 0;" 
+    allowfullscreen
+    frameborder="0"
+  ></iframe>
 `;
 
 export const Contact = () => {
@@ -82,6 +105,7 @@ export const Contact = () => {
           </div>
         </Container>
       </Section>
+      <IframeContainer dangerouslySetInnerHTML={{ __html: iframeHtml }}></IframeContainer>
       <Footer id="footer">
         <div className="container">
           <p>&copy; 2024 Ecodam Summer School</p>
